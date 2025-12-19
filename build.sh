c5af50c7649a9fd643a59ae472185ab961739068
kenaaaanz <gichabakenani@gmail.com>
2025-12-19 16:57:09 +0300

    build.sh commit 7

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # Exit on error
 set -o errexit
 
+apt-get update && apt-get install -y \
+    python3-dev \
+    build-essential \
+    libpq-dev \
+    libjpeg-dev \
+    zlib1g-dev
+
 # Install dependencies
+pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
 
 # Collect static files
